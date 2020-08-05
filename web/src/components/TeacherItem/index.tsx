4d6b2e77972e0026a37400a0389e9057c7432024
Ricardo <ricardovws@gmail.com>
2020-08-05 17:18:00 -0300

    almost done

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,35 +1,58 @@
 import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 import './styles.css';
+import api from '../../services/api';
 
-function TeacherItem(){
+
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+
+
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({teacher}) => {
+    
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id,
+        })
+    }
+    
     return (
         <article className="teacher-item">
                     <header>
-                        <img src="https://avatars1.githubusercontent.com/u/46910060?s=460&u=8c4091a27ccbf8165ba673a3a2cb4729b9b34429&v=4" alt="Ricardo Vinícius"/>
+                        <img src={teacher.avatar} alt={teacher.name}/>
                         <div>
-                            <strong>Ricardo Vinícius</strong>
-                            <span>Química</span>
+                            <strong>{teacher.name}</strong>
+                            <span>{teacher.subject}</span>
                         </div>
                     </header>
                     <p>
-                        Entusiasta das melhores tecnologias de química avançada.
-                        <br /><br />
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de esperiências.
-                        Mais de 200.000 pessoas já passaram por uma das minhas explosões.
+                        {teacher.bio}
                     </p>
 
                     <footer>
                         <p>
                             Preço/hora
                             <strong>
-                                R$ 80,00
+                                R$ {teacher.cost}
                             </strong>
                         </p>
-                        <button type="button">
+                        <a 
+                        target="_blank"
+                        onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`}>
                                 <img src={whatsappIcon} alt="Whatsapp"/>
                                 Entrar em contato
-                            </button>
+                         </a>
                     </footer>
                </article>
     )
